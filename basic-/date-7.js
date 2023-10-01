e2efb2f9f7dbf7d4a56c5119fce0add8043c77b8
shabn0349 <145366388+shabn0349@users.noreply.github.com>
2023-10-01 08:23:20 +0000

    Date

---
+++ basic-/date-7.js
@@ -0,0 +1,17 @@
+/// date 
+// const myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+
+
+
+
+// const myDate = new Date("03-01-23")
+// console.log(myDate);
+// let myTime =Date.now();
+// console.log(myTime)
+// console.log(myDate.getTime());
+let newDay = new Date()
+console.log(newDay.getMonth());
