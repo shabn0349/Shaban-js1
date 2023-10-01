23bbf042ef2eaf8d08272c065f4b1e7d023f2a5d
shabn0349 <145366388+shabn0349@users.noreply.github.com>
2023-10-01 05:02:49 +0000

    String

---
+++ basic-/Stack.js
@@ -0,0 +1,9 @@
+//First we use stack for primitive data types 
+// and heap we use for non primitive data types
+// string declaration have two techniques
+//frist
+let name = "Shaban"
+console.log(name);
+//Second way
+const consumerName = new  String("alia butt")
+console.log(consumerName)
