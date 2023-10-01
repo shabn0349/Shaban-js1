d18a05fcbc776a59ac09c81577b0437f3725bc51
shabn0349 <145366388+shabn0349@users.noreply.github.com>
2023-10-01 04:55:42 +0000

    variable

---
+++ basic-/variable-2.js
@@ -0,0 +1,12 @@
+var accountId = 1234
+let mobileNO =  "Shaban"
+const agent = 32
+s  = 100
+
+// Do not use var because it getting old hehe
+// we use let because it is related to scope 
+// const variable we use once in while 
+// Table new concept for me
+
+console.log(accountId);
+console.table([mobileNO,agent,s])
