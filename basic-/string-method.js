b55239f1cd51426080ff23d41c35d81ae04602b1
shabn0349 <145366388+shabn0349@users.noreply.github.com>
2023-10-01 05:35:37 +0000

    methods

---
+++ basic-/string-method.js
@@ -0,0 +1,7 @@
+//So  i learnt in this vedio about template letrals and string method 
+// how we decalare template letrals
+let name= "shaban"
+console.log(`The name is ${name}`);
+//some string methods here we go
+//split include  trim replace 
+//inlude mean the string is availble in that whole string
